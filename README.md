94286959900b68510508f3fd319d35685b17ce88
koki <ip.b086.koki.shinichi@gmail.com>
2019-07-30 15:57:46 +0900

    feat: chrome extensionとしてminimum機能するように

---
+++ README.md
@@ -0,0 +1,8 @@
+## How to start 
+### Edit following properties in `src/manifest.json`
+```json
+    "name": "",
+    "description": "",
+```
+### Generate icons
+### Write script
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
 module.exports = {
-  // モード値を production に設定すると最適化された状態で、
-  // development に設定するとソースマップ有効でJSファイルが出力される
-  mode: "development",
-
-  // メインとなるJavaScriptファイル（エントリーポイント）
-  entry: "./src/main.ts",
-
-  module: {
-    rules: [
-      {
-        // 拡張子 .ts の場合
-        test: /\.ts$/,
-        // TypeScript をコンパイルする
-        use: "ts-loader"
-      }
-    ]
-  },
-  // import 文で .ts ファイルを解決するため
-  resolve: {
-    extensions: [".ts"]
-  }
+    mode: "development",
+    entry: {
+        "dist/background/": "./src/background/main.ts",
+        "dist/content_scripts/": "./src/content_scripts/main.ts"
+    },
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                // 拡張子 .ts の場合
+                test: /\.ts$/,
+                // TypeScript をコンパイルする
+                use: "ts-loader"
+            }
+        ]
+    },
+    // import 文で .ts ファイルを解決するため
+    resolve: {
+        extensions: [".ts"]
+    }
 };
