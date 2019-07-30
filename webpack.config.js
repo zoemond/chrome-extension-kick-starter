d48a3e97c1fdd4bd0c68803e10ade671021e7fba
koki <ip.b086.koki.shinichi@gmail.com>
2019-07-30 16:19:16 +0900

    chore: 静的なファイルもdistに行くように

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
     mode: "development",
     entry: {
         "dist/background/": "./src/background/main.ts",
-        "dist/content_scripts/": "./src/content_scripts/main.ts"
+        "dist/content_scripts/": "./src/content_scripts/main.ts",
     },
     output: {
         path: __dirname,
-        filename: "[name]bundle.js"
+        filename: "[name]bundle.js",
     },
     module: {
         rules: [
@@ -14,12 +16,26 @@ module.exports = {
                 // 拡張子 .ts の場合
                 test: /\.ts$/,
                 // TypeScript をコンパイルする
-                use: "ts-loader"
-            }
-        ]
+                use: "ts-loader",
+            },
+        ],
     },
     // import 文で .ts ファイルを解決するため
     resolve: {
-        extensions: [".ts"]
-    }
+        extensions: [".ts"],
+    },
+    plugins: [
+        new CopyPlugin([
+            {
+                context: "./src/",
+                from: "icons",
+                to: "dist/icons",
+            },
+            {
+                context: "./src/",
+                from: "manifest.json",
+                to: "dist/manifest.json",
+            },
+        ]),
+    ],
 };
